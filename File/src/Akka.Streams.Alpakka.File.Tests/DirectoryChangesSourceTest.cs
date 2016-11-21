58c30748b4ec7fdf594e7383119b103e91efba52
Marc Piechura <piechura@gmx.net>
2016-11-21 21:30:22 +0100

    add DirectorySource

---
+++ File/src/Akka.Streams.Alpakka.File.Tests/DirectoryChangesSourceTest.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DirectoryChangesSourceTest.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Akka.Streams.Alpakka.File.Dsl;
+using Akka.Streams.Dsl;
+using Akka.Streams.TestKit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.Streams.Alpakka.File.Tests
+{
+    public class DirectoryChangesSourceTest : Akka.TestKit.Xunit2.TestKit
+    {
+        private readonly string _path;
+
+        public DirectoryChangesSourceTest(ITestOutputHelper helper) : base(output: helper)
+        {
+            Materializer = Sys.Materializer();
+
+            _path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(_path);
+        }
+
+        private ActorMaterializer Materializer { get; }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            Directory.Delete(_path, true);
+        }
+
+        [Fact]
+        public void A_DirectorySource_should_emit_on_directory_changes()
+        {
+            var probe = this.CreateSubscriberProbe<Tuple<string, WatcherChangeTypes>>();
+
+            DirectoryChangesSource.Create(_path, 200).RunWith(Sink.FromSubscriber(probe), Materializer);
+
+            probe.Request(1);
+
+            var createdFile = Path.Combine(_path, "testfile1.sample");
+            System.IO.File.WriteAllText(createdFile, string.Empty);
+            
+            var change = probe.ExpectNext();
+            Assert.Equal(change.Item1, createdFile);
+            Assert.True(change.Item2 == WatcherChangeTypes.Created);
+
+            System.IO.File.WriteAllText(createdFile, "hello");
+            change = probe.RequestNext();
+            Assert.Equal(change.Item1, createdFile);
+            Assert.True(change.Item2 == WatcherChangeTypes.Changed);
+            
+            System.IO.File.Delete(createdFile);
+            change = probe.RequestNext();
+            Assert.Equal(change.Item1, createdFile);
+            Assert.True(change.Item2 == WatcherChangeTypes.Changed);
+            change = probe.RequestNext();
+            Assert.Equal(change.Item1, createdFile);
+            Assert.True(change.Item2 == WatcherChangeTypes.Deleted);
+
+            probe.Cancel();
+        }
+    }
+}
---
+++ File/src/Akka.Streams.Alpakka.File/Dsl/DirectoryChangesSource.cs
@@ -0,0 +1,150 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DirectoryChangesSource.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2016 Akka.NET project <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Akka.Streams.Dsl;
+using Akka.Streams.Stage;
+
+namespace Akka.Streams.Alpakka.File.Dsl
+{
+    /// <summary>
+    /// Watches a file system directory and streams change events from it.
+    /// </summary>
+    public sealed class DirectoryChangesSource<T> : GraphStage<SourceShape<T>>
+    {
+        #region Logic
+
+        private sealed class Logic : GraphStageLogic, IOutHandler
+        {
+            private readonly DirectoryChangesSource<T> _stage;
+            private FileSystemWatcher _watcher;
+            private readonly Queue<T> _buffer;
+
+            public Logic(DirectoryChangesSource<T> stage) : base(stage.Shape)
+            {
+                _stage = stage;
+                _buffer = new Queue<T>();
+
+                SetHandler(stage.Out, OnPull);
+            }
+
+            public override void PreStart()
+            {
+                _watcher = new FileSystemWatcher(_stage._directoryPath);
+
+                var errorCallback = GetAsyncCallback<Exception>(FailStage);
+                _watcher.Error += (_, args) =>
+                {
+                    TryStopWatcher();
+                    errorCallback(args.GetException());
+                };
+
+                var eventCallback = GetAsyncCallback<FileSystemEventArgs>(OnEvent);
+                _watcher.Created += (_, args) => eventCallback(args);
+                _watcher.Changed += (_, args) => eventCallback(args);
+                _watcher.Deleted += (_, args) => eventCallback(args);
+                _watcher.Renamed += (_, args) => eventCallback(args);
+
+                _watcher.EnableRaisingEvents = true;
+            }
+
+            public override void PostStop() => TryStopWatcher();
+
+            private void OnEvent(FileSystemEventArgs e)
+            {
+                var element = _stage._combiner(e.FullPath, e.ChangeType);
+                _buffer.Enqueue(element);
+
+                if (_buffer.Count > _stage._maxBufferSize)
+                {
+                    TryStopWatcher();
+
+                    FailStage(
+                        new BufferOverflowException(
+                            $"Max event buffer size {_stage._maxBufferSize} reached for {_stage._directoryPath}"));
+                }
+
+                if(IsAvailable(_stage.Out))
+                    Push(_stage.Out, _buffer.Dequeue());
+            }
+            
+            private void TryStopWatcher()
+            {
+                if(_watcher == null)
+                    return;
+
+                _watcher.EnableRaisingEvents = false;
+                _watcher.Dispose();
+                _watcher = null;
+            }
+
+            public void OnPull()
+            {
+                if (_buffer.Count == 0)
+                    return;
+
+                var element = _buffer.Dequeue();
+                if (element != null)
+                    Push(_stage.Out, element);
+            }
+
+            public void OnDownstreamFinish() => TryStopWatcher();
+        }
+
+        #endregion  
+
+        private readonly string _directoryPath;
+        private readonly int _maxBufferSize;
+        private readonly Func<string, WatcherChangeTypes, T> _combiner;
+
+        /// <param name="directoryPath">Directory to watch</param>
+        /// <param name="maxBufferSize">Maximum number of buffered directory changes before the stage fails</param>
+        /// <param name="combiner">A function that combines a path and a <see cref="WatcherChangeTypes"/> into an element that will be emitted downstream</param>
+        public DirectoryChangesSource(string directoryPath, int maxBufferSize, Func<string, WatcherChangeTypes, T> combiner)
+        {
+            _directoryPath = directoryPath;
+            _maxBufferSize = maxBufferSize;
+            _combiner = combiner;
+
+            Shape = new SourceShape<T>(Out);
+        }
+
+        protected override Attributes InitialAttributes { get; } = Attributes.CreateName("DirectoryChangesSource");
+
+        public Outlet<T> Out { get; } = new Outlet<T>("DirectoryChangesSource.out");
+
+        public override SourceShape<T> Shape { get; }
+
+        protected override GraphStageLogic CreateLogic(Attributes inheritedAttributes)
+        {
+            if (!Directory.Exists(_directoryPath))
+                throw new ArgumentException($"The path {_directoryPath} does not exist");
+
+            return new Logic(this);
+        }
+
+        public override string ToString() => $"DirectoryChangesSource({_directoryPath})";
+    }
+
+    public static class DirectoryChangesSource
+    {
+        /// <summary>
+        /// API
+        /// </summary>
+        /// <param name="directoryPath">Directory to watch</param>
+        /// <param name="maxBufferSize">Maximum number of buffered directory changes before the stage fails</param>
+        /// <returns></returns>
+        public static Source<Tuple<string, WatcherChangeTypes>, NotUsed> Create(string directoryPath, int maxBufferSize)
+        {
+            var source = new DirectoryChangesSource<Tuple<string, WatcherChangeTypes>>(directoryPath, maxBufferSize, Tuple.Create);
+            return Source.FromGraph(source);
+        }
+    }
+}
